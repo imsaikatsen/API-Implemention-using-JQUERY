c5aeb070ee1d95278262aa2a13277df9cf38bae2
Padma_MAC <padma_mac@Padma-MACs-MacBook-Pro.local>
2021-06-24 14:32:21 +0600

    division to district to upazilla api implentation done

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+
+
+
 $(document).ready(function(){
 
-    load_json_data();
-
-   
-    
-   
-    
+    load_json_data(); 
    });
 
    function load_json_data()
@@ -24,34 +22,53 @@ $(document).ready(function(){
    
     }
 
+  
+        $(document).on('change', '#division', function() {
+            $('select[name="district"]').empty();
+            var divisionId = $(this).val();
+            console.log(divisionId);
+            if(divisionId) {
+                $.ajax({
+                    url: 'https://api.sdelivery.xyz/api/v1/s-delivery/districts?division_name='+divisionId,
+                    type: "GET",
+                    dataType: "json",
+                    success:function(res){
+                        console.log(res);
+                        
+                        $.each(res.data, function(key, value) {
+                            $('#district').append('<option value="'+ value.name +'">'+ value.display_name +'</option>');
+                        });
+                    }
+                });
+            }else{
+                $('select[name="district"]').empty();
+            }
+        });
+
+
+        $(document).on('change', '#district', function() {
+            $('select[name="upzilla"]').empty();
+            var districtId = $(this).val();
+            console.log(districtId);
+            if(districtId) {
+                $.ajax({
+                    url: 'https://api.sdelivery.xyz/api/v1/s-delivery/districts/'+districtId+'/upazilas',
+                    type: "GET",
+                    dataType: "json",
+                    success:function(res){
+                        console.log(res);
+                        $.each(res.data, function(key, value) {
+                            $('select[name="upzilla"]').append('<option value="'+ value.name +'">'+ value.display_name +'</option>');
+                        });
+                    }
+                });
+            }else{
+                $('select[name="upzilla"]').empty();
+            }
+        });
 
     
+       
 
 
-    $(document).on('change', '#division', function(){
-        var division_id = $(this).val();
-        if(division_id != '')
-        {
-         load_json_data('district', division_id);
-        }
-        else
-        {
-         $('#district').html('<option value="">Select district</option>');
-         $('#upzilla').html('<option value="">Select upzilla</option>');
-        }
-       });
-   
-   
-       $(document).on('change', '#district', function(){
-        var district_id = $(this).val();
-        if(district_id != '')
-        {
-         load_json_data('upzilla', district_id);
-        }
-        else
-        {
-         $('#upzilla').html('<option value="">Select upzilla</option>');
-        }
-       });
-
-
+    
